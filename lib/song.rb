f0e3262278e7bfda86b9e4c9283840fa84d16199
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-20 17:30:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,51 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :artist_name, :name
+  attr_reader :song
+
   @@all = []
+  
+    def initialize(name)
+      @name = name
+    end 
+  
+    def self.create(name)
+     @@all <<  Song.new(name)
+     return Song.new(name)
+      
+    end 
+   
+    def self.new_by_name(new_song)
+      Song.new(new_song)
+    end 
+    
+    def self.create_by_name
+      
+    end 
+    
+    def self.find_by_name
+    end 
+    
+    def self.find_or_create_by_name
+    end 
 
-  def self.all
-    @@all
-  end
+    def self.alphabetical 
+    end 
+    
+    def self.new_from_filename
+    end 
+    
+    def self.destroy_all
+    end 
+    
+    
+    
 
-  def save
-    self.class.all << self
-  end
+    def self.all
+      @@all
+    end
 
-end
+    def save
+      self.class.all << self
+    end
+
+    end
